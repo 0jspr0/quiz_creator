5c44ac98ae2dd4a4d0c0879a16686d9e074b8a6b
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-24 18:04:57 +0800

    Removed the last character of each line which is the newline

--- quiz.py
+++ quiz.py
@@ -5,3 +5,5 @@ def initialize(quiz_file):
 		lines = file.readlines()
 	number_of_entries = int(len(lines) / 6)
 	random_number = random.randrange(1, 	number_of_entries + 1)
+	for line in range(len(lines)):
+		lines[line] = lines[line][:-1]
