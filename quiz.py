588c0d8fb14ffd9c03e74000b3dd52ab9bc597df
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-24 20:03:21 +0800

    Checked if your answer is correct through your stored answer and the stored correct answer

--- quiz.py
+++ quiz.py
@@ -46,3 +46,4 @@ def load_quiz(quiz_file):
 	load_answers(random_number, lines)
 	correct_answer = load_correct_answer(random_number, lines)
 	your_answer = input_your_answer()
+	check_if_your_answer_is_correct(your_answer, correct_answer)
