eccaed1f6030cea5b080d5c41e40430c13d68da8
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-26 00:27:18 +0800

    Added the quiz objects to the function calls

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import quiz
 
 def load_quiz(quiz_file):
-	lines, random_number = initialize(quiz_file)
-	load_question(random_number, lines)
-	load_answers(random_number, lines)
-	correct_answer = load_correct_answer(random_number, lines)
-	your_answer = input_your_answer()
-	check_if_your_answer_is_correct(your_answer, correct_answer)
+	lines, random_number = quiz.initialize(quiz_file)
+	quiz.load_question(random_number, lines)
+	quiz.load_answers(random_number, lines)
+	correct_answer = quiz.load_correct_answer(random_number, lines)
+	your_answer = quiz.input_your_answer()
+	quiz.check_if_your_answer_is_correct(your_answer, correct_answer)
 
 load_quiz("quiz.txt")
