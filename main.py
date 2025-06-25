b84cb800b29206b11f4d02722ee875054aede88b
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-26 00:26:17 +0800

    Moved the load quiz function and its function calls from the quiz module to the main module

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import quiz
+
+def load_quiz(quiz_file):
+	lines, random_number = initialize(quiz_file)
+	load_question(random_number, lines)
+	load_answers(random_number, lines)
+	correct_answer = load_correct_answer(random_number, lines)
+	your_answer = input_your_answer()
+	check_if_your_answer_is_correct(your_answer, correct_answer)
+
+load_quiz("quiz.txt")
